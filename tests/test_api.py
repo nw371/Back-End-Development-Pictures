77e4575c7e94234b552bd76f11fa7e30c3fd68b4
nw371 <sergey@batalov.email>
2025-05-02 08:13:03 -0400

    implemented pictures service

--- tests/test_api.py
+++ tests/test_api.py
@@ -64,9 +64,9 @@ def test_update_picture_by_id(client, picture):
     res = client.get(f'/picture/{id}')
     res_picture = res.json
     assert res_picture['id'] == 2
-    res_state = res_picture["event_state"]
+    res_state = res_picture['event_state']
     new_state = "*" + res_state
-    res_picture["event_state"] = new_state
+    res_picture['event_state'] = new_state
     res = client.put(f'/picture/{id}', data=json.dumps(res_picture),
                      content_type="application/json")
     res.status_code == 200
